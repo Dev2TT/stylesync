ecf50a699ed01142e04e6c7750083d40b98a0d59
Dev2TT <victtor.viltrumita@gmail.com>
2026-02-09 21:23:54 -0300

    Criando arquitetura do projeto e Definindo o Blueprint

---
+++ app/routes/main.py
@@ -0,0 +1,15 @@
+from flask import Blueprint,jsonify
+
+main_bp=Blueprint('main_bp',__name__)
+
+@main_bp.route('/')
+def index():
+    return jsonify({"message":"Bem vindo ao StyleSync!"})
+
+@main_bp.route('/products')
+def get_products():
+    return jsonify({"message":"Esta é a rota de listagem dos produtos"})
+
+@main_bp.route('/login',methods=['POST'])
+def login():
+    return jsonify({'message':'Rota de Login!'})
